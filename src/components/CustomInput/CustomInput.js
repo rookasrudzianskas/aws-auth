8b59ba285dc8f8b9f63f066d47eeb501ac612887
Rokas <rookas.rudzenskas@gmail.com>
2022-02-19 11:07:25 +0200

    Rokas

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {Text, View, StyleSheet, TextInput} from 'react-native';
 import tw from "tailwind-react-native-classnames";
 
-const CustomInput = () => {
+const CustomInput = ({value, setValue, placeholder}) => {
     return (
-        <View style={tw`bg-white`}>
-            <TextInput placeholder="This is placeholder" />
+        <View style={tw`bg-white flex w-full border border-gray-200 rounded-sm px-5 my-5 justify-center flex-1`}>
+            <TextInput
+                value={value}
+                onChangeText={setValue}
+                placeholder={placeholder} />
         </View>
     );
 };
--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, Image, useWindowDimensions} from 'react-native';
 import Logo from '../../../assets/images/Logo_1.png';
 import tw from "tailwind-react-native-classnames";
@@ -6,11 +6,14 @@ import CustomInput from "../../components/CustomInput/CustomInput";
 
 const SignInScreen = () => {
     const {height} = useWindowDimensions();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
 
     return (
         <View style={tw`flex items-center p-5`}>
             <Image style={[styles.logo, {height: height * 0.3}]} resizeMode="contain" source={Logo} />
-            <CustomInput />
+            <CustomInput placeholder={"Username"} value={username} setValue={setUsername}  />
+            <CustomInput placeholder={"Password"} value={password} setValue={setPassword} />
         </View>
     );
 };
