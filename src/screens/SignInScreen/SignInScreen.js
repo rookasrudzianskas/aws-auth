22569f7eb2fcd358bdbae86eb5e98956a23fa888
Rokas <rookas.rudzenskas@gmail.com>
2022-02-19 10:37:17 +0200

    Rokas

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const SignInScreen = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default SignInScreen;
